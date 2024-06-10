5397cd0e30bbde2ec78968b6c5f65a36703b340f
Andras Molnar <molnarandris@gmail.com>
2024-06-10 09:29:32 +0200

    Popover modifications

--- texwriter/autocomplete.py
+++ texwriter/autocomplete.py
@@ -5,57 +5,88 @@ from gi.repository import GObject
 class AutocompletePopover(Gtk.Popover):
     __gtype_name__ = 'AutocompletePopover'
 
-    __gsignals__ = {
-        'update-position': (GObject.SIGNAL_RUN_FIRST, None, ()),
-    }
-
     def __init__(self, parent):
         super().__init__()
         self.set_parent(parent)
-
-        controller = Gtk.EventControllerKey()
-        controller.connect("key_released", self.key_release_cb)
-        parent.add_controller(controller)
-        self.set_autohide(True)
+        self.set_autohide(False)
         self.is_active = False
-
-        #controller = Gtk.GestureClick()
-        #controller.connect("released", self.button_release_cb)
-        #parent.get_root().add_controller(controller)
-
         self.listbox = Gtk.ListBox()
         self.set_child(self.listbox)
 
+        controller = Gtk.EventControllerKey()
+        controller.connect("key_pressed", self.key_press_cb)
+        parent.add_controller(controller)
+
+        controller = Gtk.GestureClick()
+        controller.connect("released", self.button_release_cb)
+        parent.get_root().add_controller(controller)
+
         # This is an example to fill up the rows in the popover
         row = Gtk.ListBoxRow()
-        row.text = "Hello pop 1"
+        row.text = "\\alpha"
         row.set_child(Gtk.Label.new(row.text))
         self.listbox.append(row)
         row = Gtk.ListBoxRow()
-        row.text = "Hello pop 2"
+        row.text = "\\beta"
         row.set_child(Gtk.Label.new(row.text))
         self.listbox.append(row)
 
-    def key_release_cb(self, controller, keyval, keycode, state):
+    def key_press_cb(self, controller, keyval, keycode, state):
         if not self.is_active and keyval != Gdk.KEY_backslash: return
-        self.emit("update-position")
-        if keyval == Gdk.KEY_backslash:
-            self.popup()
-            row = self.listbox.get_row_at_index(0)
-            self.listbox.select_row(row)
-            self.is_active = True
-        if keyval == Gdk.KEY_Return:
-            row = self.listbox.get_selected_row()
-            if not row:
-                self.popdown()
-                self.is_active = False
+        match keyval:
+            case Gdk.KEY_backslash:
+                self.activate()
+                return False
+            case Gdk.KEY_Escape:
+                self.deactivate()
+                return True
+            case Gdk.KEY_Return:
+                row = self.listbox.get_selected_row()
+                if not row:
+                    self.deactivate()
+                return True
+            case Gdk.KEY_Down:
+                self.select_next_row()
+                return True
+            case Gdk.KEY_Up:
+                self.select_prev_row()
+                return True
+            case _:
+                self.update_position()
 
-        if keyval == Gdk.KEY_Escape:
-            self.popdown()
-            self.is_active = False
+    def activate(self):
+        self.update_position()
+        self.popup()
+        row = self.listbox.get_row_at_index(0)
+        self.listbox.select_row(row)
+        self.is_active = True
+
+    def deactivate(self):
+        self.popdown()
+        self.is_active = False
+
+    def select_next_row(self):
+        n = self.listbox.get_selected_row().get_index()
+        row = self.listbox.get_row_at_index(n+1)
+        if row: self.listbox.select_row(row)
+
+    def select_prev_row(self):
+        n = self.listbox.get_selected_row().get_index()
+        row = self.listbox.get_row_at_index(n-1)
+        if row: self.listbox.select_row(row)
 
     def button_release_cb(self, window, n, x, y):
         if not self.is_active: return
-        self.popdown()
+        self.deactivate()
 
-
+    def update_position(self):
+        textview = self.get_parent()
+        buffer = textview.get_buffer()
+        it = buffer.get_iter_at_mark(buffer.get_insert())
+        buf_rect = textview.get_iter_location(it)
+        rect = Gdk.Rectangle()
+        x, y = textview.buffer_to_window_coords(Gtk.TextWindowType.TEXT, buf_rect.x,buf_rect.y)
+        rect.x, rect.y = textview.translate_coordinates(textview, x,y)
+        rect.width = buf_rect.width
+        rect.height = buf_rect.height
+        self.set_pointing_to(rect)
--- texwriter/window.py
+++ texwriter/window.py
@@ -97,10 +97,7 @@ class TexwriterWindow(Adw.ApplicationWindow):
         self.logview.connect("row-activated", lambda _, row: self.scroll_to(row.line))
         self.pdf_log_switch.connect("clicked", self.pdf_log_switch_cb)
 
-        # Here I should subclass textview/sourceview....
         self.popover = AutocompletePopover(self.textview)
-        self.popover.connect("update-position", self.update_popover_position)
-
 
     def open_document(self, _action, _value):
 
@@ -379,13 +376,3 @@ class TexwriterWindow(Adw.ApplicationWindow):
             case _:
                 logger.warning("Pdf log switch button clicked while stack is not visible")
 
-    def update_popover_position(self, popover):
-        buffer = self.textview.get_buffer()
-        it = buffer.get_iter_at_mark(buffer.get_insert())
-        buf_rect = self.textview.get_iter_location(it)
-        rect = Gdk.Rectangle()
-        x, y = self.textview.buffer_to_window_coords(Gtk.TextWindowType.TEXT, buf_rect.x,buf_rect.y)
-        rect.x, rect.y = self.textview.translate_coordinates(self.textview, x,y)
-        rect.width = buf_rect.width
-        rect.height = buf_rect.height
-        popover.set_pointing_to(rect)
