0504041f894b547eb0ae0616c5aa57818d0332cc
Andras Molnar <molnarandris@gmail.com>
2024-06-11 09:30:42 +0200

    Autocomplete: filter, select and activate rows

--- texwriter/autocomplete.py
+++ texwriter/autocomplete.py
@@ -17,6 +17,10 @@ class AutocompletePopover(Gtk.Popover):
         controller.connect("key_pressed", self.key_press_cb)
         parent.add_controller(controller)
 
+        controller = Gtk.EventControllerKey()
+        controller.connect("key_released", self.key_release_cb)
+        parent.add_controller(controller)
+
         controller = Gtk.GestureClick()
         controller.connect("released", self.button_release_cb)
         parent.get_root().add_controller(controller)
@@ -30,6 +34,7 @@ class AutocompletePopover(Gtk.Popover):
         row.text = "\\beta"
         row.set_child(Gtk.Label.new(row.text))
         self.listbox.append(row)
+        self.selected_row = None
 
     def key_press_cb(self, controller, keyval, keycode, state):
         if not self.is_active and keyval != Gdk.KEY_backslash: return
@@ -37,13 +42,11 @@ class AutocompletePopover(Gtk.Popover):
             case Gdk.KEY_backslash:
                 self.activate()
                 return False
-            case Gdk.KEY_Escape:
+            case Gdk.KEY_Escape | Gdk.KEY_space:
                 self.deactivate()
-                return True
-            case Gdk.KEY_Return:
-                row = self.listbox.get_selected_row()
-                if not row:
-                    self.deactivate()
+                return False
+            case Gdk.KEY_Return | Gdk.KEY_Tab:
+                self.activate_selected_row()
                 return True
             case Gdk.KEY_Down:
                 self.select_next_row()
@@ -52,7 +55,24 @@ class AutocompletePopover(Gtk.Popover):
                 self.select_prev_row()
                 return True
             case _:
-                self.update_position()
+                return False
+
+    def key_release_cb(self, controller, keyval, keycode, state):
+        if keyval in [Gdk.KEY_Down, Gdk.KEY_Down]:
+            return
+        if not self.is_active: return
+        self.listbox.invalidate_filter()
+        self.listbox.set_filter_func(self.filter_func)
+        empty = True
+        for row in self.listbox:
+            if self.filter_func(row):
+                self.listbox.select_row(row)
+                empty = False
+                break
+        if empty:
+            self.deactivate()
+        else:
+            self.update_position()
 
     def activate(self):
         self.update_position()
@@ -60,10 +80,39 @@ class AutocompletePopover(Gtk.Popover):
         row = self.listbox.get_row_at_index(0)
         self.listbox.select_row(row)
         self.is_active = True
+        buffer = self.get_parent().get_buffer()
+        it = buffer.get_iter_at_mark(buffer.get_insert())
+        mark = Gtk.TextMark.new("autocomplete", left_gravity=True)
+        buffer.add_mark(mark,it)
 
     def deactivate(self):
         self.popdown()
+        buffer = self.get_parent().get_buffer()
+        mark = buffer.get_mark("autocomplete")
+        buffer.delete_mark(mark)
         self.is_active = False
+
+    def activate_selected_row(self):
+        row = self.listbox.get_selected_row()
+        buffer = self.get_parent().get_buffer()
+        text = self.get_typed_text()
+        buffer.insert_at_cursor(row.text.lstrip(text))
+        self.deactivate()
+
+    def get_typed_text(self):
+        buffer = self.get_parent().get_buffer()
+        mark = buffer.get_mark("autocomplete")
+        start_it = buffer.get_iter_at_mark(mark)
+        end_it = buffer.get_iter_at_mark(buffer.get_insert())
+        text = buffer.get_text(start_it, end_it, include_hidden_chars=False)
+        return text
+
+    def filter_func(self, row):
+        text = self.get_typed_text()
+        if row.text.find(text) == -1:
+            return False
+        else:
+            return True
 
     def select_next_row(self):
         n = self.listbox.get_selected_row().get_index()
